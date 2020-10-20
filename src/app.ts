8ddf869e1751a2f2f7e7df18b39526fdf1210cf7
papasavvas <papasavva@protonmail.com>
2020-10-20 22:50:33 +0200

    Added error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-import express from 'express'
+import express, { Request, Response, NextFunction} from 'express'
+
+import todoRoutes from './routes/todo'
 
 const app = express()
 
+app.use('/routes', todoRoutes)
+
+//error handling
+app.use((err: Error,req:Request, res: Response,next:NextFunction)=> {
+    res.status(500).json({ message : err.message})
+})
 app.listen(3000)
