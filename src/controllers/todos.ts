9d076b4ab65c368cb2d4a7a2822c004bc9411934
papasavvas <papasavva@protonmail.com>
2020-10-20 23:46:51 +0200

    Finished CRUD

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -3,7 +3,7 @@ import {RequestHandler} from 'express'
 import {Todo} from '../models/todos'
 const TODOS : Todo[]= []
 
-export const create : RequestHandler = (req, res, next) => {
+export const createTodo : RequestHandler = (req, res, next) => {
     const id = Math.random().toString()
     const text = (req.body as {text : string}).text
 
@@ -14,11 +14,11 @@ export const create : RequestHandler = (req, res, next) => {
     res.json({message : 'Created the todo', data : newTodo})
 }
 
-export const read : RequestHandler = (req, res, next) => {
+export const readTodo : RequestHandler = (req, res, next) => {
     res.json({message : 'List of todos', data : TODOS})
 }
 
-export const update : RequestHandler<{id:string}> = (req, res, next) => {
+export const updateTodo : RequestHandler<{id:string}> = (req, res, next) => {
     const id = req.params.id
     const updatedText = (req.body as {text : string}).text
     const todoIndex = TODOS.findIndex(todo => todo.id === id)
@@ -31,4 +31,20 @@ export const update : RequestHandler<{id:string}> = (req, res, next) => {
 
 
     res.json({message : 'Todo is updated', data : TODOS[todoIndex]})
+}
+
+
+export const deleteTodo : RequestHandler = (req, res, next) => {
+    const id = req.params.id
+    const todoIndex = TODOS.findIndex(todo => todo.id === id)
+
+    if (todoIndex < 0)  {
+        throw new Error('No such todo with this id.')
+    }
+
+    const removedTodo = TODOS[todoIndex]
+    TODOS.splice(todoIndex,1)
+
+
+    res.json({message : 'Todo is deleted', data : removedTodo})
 }
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,12 +1,12 @@
 import {Router} from "express"
 
-import {create, read, update} from '../controllers/todos'
+import {createTodo, readTodo, updateTodo, deleteTodo} from '../controllers/todos'
 
 const router = Router()
 
-router.post('/', create)
-router.get('/', read)
-router.patch('/:id', update)
-router.delete('/:id')
+router.post('/', createTodo)
+router.get('/', readTodo)
+router.patch('/:id', updateTodo)
+router.delete('/:id', deleteTodo)
 
 export default router
